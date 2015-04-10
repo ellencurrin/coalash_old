1d7cbc690315dd0ea16c68331240f8119b2c6258
itrc <itrc@itrc-pc.local>
2015-04-10 12:25:25 -0400

    in progress: linking ponds to plants

--- js/coalash.js
+++ js/coalash.js
@@ -16,7 +16,8 @@ function parseData(data){
 var map
 var states
 var ponds
-var points = L.geoJson()
+var plants
+//var points = L.geoJson()
 var red = "#A82904"
 var yellow = "#D9AD24"
 var green = "#88AD40"
@@ -52,18 +53,18 @@ function buildMap() {
         //map.scrollWheelZoom.disable();
     
     /// BASE MAP    
-    var Acetate_basemap = L.tileLayer('http://a{s}.acetate.geoiq.com/tiles/acetate-base/{z}/{x}/{y}.png', {
+    var acetate = L.tileLayer('http://a{s}.acetate.geoiq.com/tiles/acetate-base/{z}/{x}/{y}.png', {
 	attribution: '&copy;2012 Esri & Stamen, Data from OSM and Natural Earth',
 	subdomains: '0123',
 	minZoom: 2,
 	maxZoom: 18
     })
-    //.addTo(map);
+    .addTo(map);
     
     var osm_BW = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     })
-    .addTo(map);
+    //.addTo(map);
     
     var imagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
@@ -76,9 +77,9 @@ function buildMap() {
             this.eachLayer(function(polygon){
                 polygon.setStyle ({
                     color: '#E3E3DD',
-                    weight: 5,
+                    weight: 1,
                     fillColor: '#E3E3DD',//'#C3C3BE',
-                    fillOpacity: .7,
+                    fillOpacity: .9,
                 })    
             })
         })
@@ -96,13 +97,13 @@ function buildMap() {
                                     fillOpacity: .4
                     });
                     polygon.on('click', function(e){    
-                        console.log(polygon.getBounds())
                         map.fitBounds(polygon.getBounds())
-                        //map.addLayer(osm_BW)
-                        map.addLayer(plants)
+                        map.addLayer(plants);
+                        map.removeLayer(acetate);
+                        map.addLayer(osm_BW);
                         e.layer.setStyle({
                             weight: 3,
-                            fillOpacity: 0.2,
+                            fillOpacity: 0,
                         })
                     }) 
                 })
@@ -110,12 +111,16 @@ function buildMap() {
         .addTo(map);
         
         states.on('mouseover', function(e) {
-            if (map.getZoom()<=13) {
+            if (map.getZoom()<=6) {
                 e.layer.setStyle({
                     weight: 3,
                     fillOpacity: 0.2, 
                 });
             } else {
+                states.setStyle ({
+                    weight: 2, 
+                    fillOpacity: 0.4, 
+                })
                 e.layer.setStyle ( {
                     weight: 2, 
                     fillOpacity: 0, 
@@ -123,7 +128,7 @@ function buildMap() {
             }
         });
         states.on('mouseout', function(e) {
-            if (map.getZoom()<=13) {
+            if (map.getZoom()<=6) {
                 e.layer.setStyle ( {
                     weight: 2, 
                     fillOpacity: 0.4, 
@@ -140,20 +145,45 @@ function buildMap() {
     plants = omnivore.geojson('data/plants.geojson')
     .on('ready', function(go){
         this.eachLayer(function(marker) {
-            marker.on('click', function(e){
-                console.log(e)
-                map.setView(e.latlng, 14)
-                map.removeLayer(plants)
-                map.removeLayer(osm_BW)
-                map.addLayer(imagery)
-                map.addLayer(ponds)
+            var plant = marker.feature.properties.label
+            var gal = 0
+            var condition = 0
+            var count = 0
+            var value = 0
+            omnivore.geojson('data/coal_ash_impoundments_selc.geojson')
+            .on('ready', function(go) {
+                this.eachLayer(function(polygon) {
+                    if (polygon.feature.properties.plant_labe == plant) {
+                        count += 1
+                        gal += polygon.feature.properties.gallons
+                        if (polygon.feature.properties.epa_con_as == "Poor") {
+                            value += 3;
+                        } else if (polygon.feature.properties.epa_con_as == "Fair") {
+                            value += 2;
+                        } else if (polygon.feature.properties.epa_con_as == "Satisfactory") {
+                            value += 1;
+                        } else {
+                            value += 0;
+                        }
+                        condition += value
+                    }
+                    console.log(condition/count)
+                })
             })
-            var color 
-            if (marker.feature.properties.contamin_1 == 'Yes') {
+            var color
+            console.log(condition/count)
+            if (condition/count > 3) {
+                color = "#fff";
+            } else if (condition/count > 1.5) {
+                color = yellow;
+            } else if (condition/count > 0) {
+                color = green;
+            } else {color="#594736"}
+           /* if (marker.feature.properties.contamin_1 == 'Yes') {
                 color = red
             } else {
                 color = yellow
-            }
+            }*/
             marker.setIcon(L.divIcon( {
                 iconSize: [1, 1],
                 popupAnchor: [0, 10], 
@@ -162,19 +192,28 @@ function buildMap() {
             }))
             var label = marker.feature.properties.label
             marker.bindLabel(label)
+            
+            marker.on('click', function(e){
+                console.log(e)
+                map.setView(e.latlng, 14)
+                map.removeLayer(plants)
+                map.removeLayer(osm_BW)
+                map.addLayer(imagery)
+                map.addLayer(ponds)
+            })
         })
     })
-    //.addTo(map)
+    
+    /// CREATING PLAN GEOJSON
+    //plants = L.geoJson().addTo(map);
+
     
     /// ADD PONDS + CREATE PLANTS
-    ponds = omnivore.geojson('data/impoundments_selc.geojson')
+    ponds = omnivore.geojson('data/coal_ash_impoundments_selc.geojson')
     .on('ready', function(go) {
         console.log(go)
-        var ONE   
-        var TWO
-        var ll
-        var counter
-        this.eachLayer(function(polygon) {
+        this.eachLayer(function(polygon) {            
+            /// Set Style
             if (polygon.feature.properties.epa_con_as == "Poor") {
                 polygon.setStyle ( {
                             color: red, 
@@ -229,11 +268,12 @@ function buildMap() {
                 console.log("zoom greater than or equal 13")
                 //map.removeLayer(osm_BW);
                 map.addLayer(imagery);
-            } else if (map.getZoom()<=9){
-                console.log("zoom less than 13")
-                map.removeLayer(imagery);
-                map.addLayer(osm_BW);
+            }else if (map.getZoom()<=6){
+                console.log("zoom less than 6")
+                map.removeLayer(osm_BW);
+                map.addLayer(acetate);
                 map.removeLayer(ponds);
+                map.removeLayer(plants);
             }
     })
     
@@ -248,4 +288,69 @@ function resetExtent(){
     map.removeLayer(plants)
     map.removeLayer(ponds)
     
+}
+
+function concatPlant(polygon, ll, gal, condition, counter, ONE) {
+    console.log("concat plant")
+    ONE = polygon.feature.properties.plant_labe
+    ll.push([Number(polygon.feature.properties.latitude), Number(polygon.feature.properties.longitude)])
+    //console.log(ll)
+    gal += polygon.feature.properties.gallons
+    var value
+    if (polygon.feature.properties.epa_con_as == "Poor") {
+        value = 3;
+    } else if (polygon.feature.properties.epa_con_as == "Fair") {
+        value = 2;
+    } else if (polygon.feature.properties.epa_con_as == "Satisfactory") {
+        value = 1;
+    } else {
+        value = 0;
+    }
+    condition += value
+    var TWO = polygon.feature.properties.plant_labe
+    console.log(TWO)
+    return ll, gal, condition, counter, TWO
+}
+
+function newPlant(polygon, ll, gal, condition, counter, ONE){
+    console.log("creating new plant")
+    //console.log(ll)
+    ONE = polygon.feature.properties.plant_labe
+    if (ll.length == 0) {
+        ll = [Number(polygon.feature.properties.longitude), Number(polygon.feature.properties.latitude)]
+        type = "Point"
+    } else {
+        type = "Polygon"
+    }
+    var geojsonFeature = {
+        "type": "Feature",
+        "properties": {
+            "tot_gallons": gal,
+            "con_value": condition,
+            "num_ponds": counter
+        },
+        "geometry": {
+            "type": type,
+            "coordinates": ll
+        }
+    };
+    plants.addData(geojsonFeature);
+    
+    var TWO = polygon.feature.properties.plant_labe
+    console.log(TWO)
+    ll = [Number(polygon.feature.properties.longitude), Number(polygon.feature.properties.latitude)]
+    gal = polygon.feature.properties.gallons
+    counter = 1
+    var value
+    if (polygon.feature.properties.epa_con_as == "Poor") {
+        value = 3;
+    } else if (polygon.feature.properties.epa_con_as == "Fair") {
+        value = 2;
+    } else if (polygon.feature.properties.epa_con_as == "Satisfactory") {
+        value = 1;
+    } else {
+        value = 0;
+    }
+    condition = value
+    return ll, gal, condition, counter, TWO
 }
